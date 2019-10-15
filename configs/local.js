8b0dc98f8ed60ea070bdb2fafd05e5b65a63f8f4
Rafael-Deiro <rafaeldeirolopes@gmail.com>
2019-10-15 01:19:14 -0300

    Removed source code at ./ The remainder source code at ./ directory was removed

--- configs/local.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const localConfig = { hostname: '127.0.0.1', port: 2207 };
-
-module.exports = localConfig;
--- index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const server = require('./server');
-const configs = require('./configs');
-const db = require('./configs/db');
-
-server.create(configs, db);
-server.start();
