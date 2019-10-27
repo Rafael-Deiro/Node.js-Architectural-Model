dff97a57f6346940c01de1c0c07ff030a8998c1d
Rafael-Deiro <rafaeldeirolopes@gmail.com>
2019-10-27 20:55:07 -0300

    Added built app to Heroku branch

---
+++ build/config/database.js
@@ -0,0 +1,26 @@
+'use strict';
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv = __importStar(require("dotenv"));
+dotenv.config();
+exports.production = {
+    use_env_variable: 'DATABASE_URL',
+    url: process.env.DATABASE_URL || process.env.PROD_DB_URL,
+    dialect: process.env.PROD_DB_DIALECT,
+    migrationStorage: process.env.PROD_DB_MIGRATIONSTORAGE,
+    migrationStoragePath: process.env.PROD_DB_MIGRATIONSTORAGEPATH
+};
+exports.development = {
+    use_env_variable: 'DATABASE_URL',
+    url: process.env.DATABASE_URL || process.env.DEV_DB_URL,
+    dialect: process.env.DEV_DB_DIALECT,
+    migrationStorage: process.env.DEV_DB_MIGRATIONSTORAGE,
+    migrationStoragePath: process.env.DEV_DB_MIGRATIONSTORAGEPATH
+};
+//# sourceMappingURL=database.js.map
